e738b1d6ea970c70a37be364f72676d6a74db8e0
Ignacio Torres Masdeu <i@itorres.net>
2015-03-13 02:19:37 +0100

    Remove build.sh

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-checkout=$(dirname $(readlink -f $0))
-wwwroot=do.accela.cat:/srv/docker/nginx/itorres.net/site/
-echo docker run --rm -v "${checkout}:/src" grahamc/jekyll build
-echo rsync -lav ${checkout}/_site/ $wwwroot
