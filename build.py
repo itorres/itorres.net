7ecac30ba149cd1f1f119a4272406107c4f6e893
Ignacio Torres Masdeu <i@itorres.net>
2016-02-28 21:50:03 +0100

    Clean branch all_posts

--- build.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-import os,sys
-checkout=os.path.dirname(os.path.realpath(__file__))
-wwwroot="root@itorres.net:/srv/docker/nginx/itorres.net/site/"
-print 'docker run --rm -v "{checkout}:/src" grahamc/jekyll build'.format(checkout=checkout)
-print 'rsync -xHlav "{checkout}/_site/" "{wwwroot}"'.format(checkout=checkout, wwwroot=wwwroot)
-
